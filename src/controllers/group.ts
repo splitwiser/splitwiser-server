aff75843b86af434ee74d531f47d0b69ada4f9c1
Akash NP <akashnp1998@gmail.com>
2023-11-20 10:03:37 +0000

    chore: setup lint-staged for auto formatting and linting

--- src/controllers/group.ts
+++ src/controllers/group.ts
@@ -1,9 +1,9 @@
 import type { Request, Response } from "express";
-
+import type { Group } from "../types/group.js";
 import { listGroups } from "../services/group.js";
 
 export async function listGroupsController(req: Request, res: Response) {
   const { memberId } = req.params;
-  const groups: any = await listGroups(memberId)
-  return res.status(200).json(groups)
+  const groups: Group[] = await listGroups(memberId);
+  return res.status(200).json(groups);
 }
