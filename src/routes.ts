1402da7305512167d85e0f1f04334ccad6d622e6
Akash NP <akashnp1998@gmail.com>
2023-11-23 23:20:24 +0400

    chore: fix imports

--- src/routes.ts
+++ src/routes.ts
@@ -1,20 +1,19 @@
 import express from "express";
 import { listGroupsController } from "./controllers/group.js";
-import {
-  getFriendBalanceController,
-  listFriendsController,
-  listFriendsTransactionsController,
-} from "./controllers/user.js";
+
+import listFriendsController from "./controllers/friends/listFriends/index.js";
+import { listExpensesWithFriendController } from "./controllers/friends/listExpensesWithFriend/index.js";
+import { getBalanceWithFriendController } from "./controllers/friends/getBalanceWithFriend/index.js";
 
 export const router = express.Router();
 
 router.get("/group", listGroupsController);
 router.get("/users/:userId/friends", listFriendsController);
 router.get(
-  "/users/:userId/friends/:friendId/transactions",
-  listFriendsTransactionsController,
+  "/users/:userId/friends/:friendId/expenses",
+  listExpensesWithFriendController,
 );
 router.get(
   "/users/:userId/friends/:friendId/balance",
-  getFriendBalanceController,
+  getBalanceWithFriendController,
 );
