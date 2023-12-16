ca9957844f20114915d0898fd033d53daf8b7d42
Akash NP <akashnp1998@gmail.com>
2023-12-16 13:52:55 +0400

    chore: list friends with balances
    uses a fixed rpc function that takes into consideration the two
    way relation of user friends

--- src/controllers/friends/listFriends/index.ts
+++ src/controllers/friends/listFriends/index.ts
@@ -1,13 +1,22 @@
 import type { Request, Response } from "express";
 import { createSuccessResponse } from "@9akashnp8/express-response-module";
 
-import { listFriends } from "../../../services/user.js";
+import {
+  listFriends,
+  listFriendsWithBalance,
+} from "../../../services/friends.js";
 
 export default async function listFriendsController(
   req: Request,
   res: Response,
 ) {
+  let friends = [];
   const { userId } = req.params;
-  const friends = await listFriends(userId!);
+  const { withBalance } = req.query;
+  if (withBalance === "True" || withBalance === "true") {
+    friends = await listFriendsWithBalance(userId!);
+  } else {
+    friends = await listFriends(userId!);
+  }
   return createSuccessResponse(res, 200, friends);
 }
